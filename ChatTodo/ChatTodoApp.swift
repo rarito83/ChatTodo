50c94ca0a7f43c8b2a2bfb3e13a5718407031699
rarito83 <rarito.jogja@gmail.com>
2023-09-05 09:52:48 +0700

    Initial Commit

---
+++ ChatTodo/ChatTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatTodoApp.swift
+//  ChatTodo
+//
+//  Created by Rarito on 05/09/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
